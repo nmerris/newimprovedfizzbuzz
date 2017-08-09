c95b91b7431cd7e200601843829be772e9bb5d49
nmerris <nate.merris@gmail.com>
2017-08-09 13:35:24 -0400

    initial commit with bootstrap template unaltered

---
+++ src/main/java/com/nmerris/newimprovedfb/NewimprovedfbApplication.java
@@ -0,0 +1,12 @@
+package com.nmerris.newimprovedfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewimprovedfbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewimprovedfbApplication.class, args);
+	}
+}
